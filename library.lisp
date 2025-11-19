1dbde8aa29e0042e44782fac239457d85c7124eb
Sungmin Park <byulparan@gmail.com>
2025-11-19 16:55:14 +0900

    Fetch and build webview(https://github.com/webview/webview)

--- library.lisp
+++ library.lisp
@@ -4,12 +4,17 @@
 ;; 
 
 
-#+windows
-(pushnew '(merge-pathnames "lib/" (asdf:system-source-directory :cl-webview)) cffi:*foreign-library-directories*)
+;; If the lib directory does not exist in cl-webview, then fetch webview from GitHub and build it.
+(unless (probe-file (asdf:system-relative-pathname :cl-webview  "lib"))
+  (uiop:run-program (format nil "cd ~a && rm -rf build && cmake -B build && make -C build" (namestring (asdf:system-source-directory :cl-webview)))
+		    :output :interactive))
+
+(pushnew '(asdf:system-relative-pathname :cl-webview "lib/") cffi:*foreign-library-directories*)
 
 
 (cffi:define-foreign-library libwebview
   (:darwin "libwebview.dylib")
+  (:unix "libwebview.so")
   (:windows "webviewd.dll"))
 
 (cffi:use-foreign-library libwebview)
